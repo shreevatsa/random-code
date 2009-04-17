8718cffbe4c49a24b6466611e9c0f3c0c284b28b
shreevatsa <shreevatsa@09792f66-1188-11de-8ac1-5b40c4bb5d67>
2009-04-17 00:58:39 +0000

    Adding someone else's script that removes accesskeys

---
+++ trunk/accesskeys.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name          Remove access keys
+// @namespace     http://code.google.com/p/random-code/
+// ==/UserScript==
+//
+
+/*
+  From http://userscripts.org/scripts/show/18306 by zzo38:
+  http://zzo38computer.cjb.net/userjs/
+  I just changed the keys.
+*/
+
+r=document.evaluate("//*[@accesskey]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+for(i=0;i<r.snapshotLength;i++) {
+  item=r.snapshotItem(i);
+  k=item.getAttribute("accesskey");
+  if(k.match(/^[fbaenpd]$/i)) item.removeAttribute("accesskey");
+}
