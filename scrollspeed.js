5bf0844a55ca2bd2b72402e7b830564e884e19b7
shreevatsa <shreevatsa@09792f66-1188-11de-8ac1-5b40c4bb5d67>
2010-02-06 15:56:08 +0000

    Uploaded version

--- scrollspeed.js
+++ scrollspeed.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name          ScrollSpeed (auto-scroll)
-// @description   Automatically scrolls page so as to end by a specified time
-// @namespace     
-// @version       0.1
+// @description   Automatically scrolls page to end by a specified time
+// @namespace     http://code.google.com/p/random-code/
+// @version       0.2
 // @include       *
 // ==/UserScript==
 //
@@ -11,30 +11,47 @@
 //
 /*
 
+  Purpose
+  =======
+  Imagine you're reading a long webpage (e.g. a novel!) that you don't
+  care to read too carefully, but want to finish in, say, two hours.
+
   Usage
   =====
   Scroll down to the end of the page/region you want to read,
   and choose "Set_bottom" in the Greasemonkey User Script Commands.
 
+  A text box should appear at the top right corner of the window.
+  Enter a number (of minutes) in the box, and click on the button.
+
   Now scroll up. The page will scroll automatically at the proper rate
-  to reach the end in the given time. Good luck reading.
+  (shown in button), to reach the end in the given time.
+
+  Good luck reading.
+
+  (You can enter another number and click again to change the end time.
+  To make it stop entirely, enter a very large number, e.g. 99999.)
 
   Notes
   =====
+  * The scroll rate, instead of remaining constant, tends to increase.
+  This is probably because Firefox can only scroll by discrete amounts.
+  I'm not sure how to fix this. In the meantime, set it to end earlier
+  than you need it to.
 
   * If the horizontal (x) scroll position of the final state (when you
   "Set_bottom") is not the same as the current, it scrolls horizontally
-  as well.  This may not be what you want.
+  as well.  This is a "feature", but it may not be what you want.
 
   Changelog:
 
-  2010-02-06 v0.2    First working version
-  2010-01-15 v0.1    First version
+  2010-02-06 v0.1    First working version
+  2010-01-15 v0.0    First version
 */
 
 "use strict";
 /*jslint browser: true, onevar: false, white:false, plusplus: false, undef: true, eqeqeq: true*/
-/*global window, document, alert, GM_registerMenuCommand, GM_log, $ */
+/*global window, document, alert, GM_registerMenuCommand, GM_log */
 
 if(window===window.top) {
   (function() {
@@ -51,8 +68,8 @@ if(window===window.top) {
       if(things_to_do.length>0) { things_to_do.shift()(); }
       window.setTimeout(pop_queue, eps);
     }
-    //pop_queue(); //Get it going
 
+    /*
     function toWidth(w, n) {
       s = '' + n;
       while(s.length < w) s = '0' + s;
@@ -62,17 +79,18 @@ if(window===window.top) {
       var d = new Date(ms);
       return d.getUTCHours() + ':' + toWidth(2,d.getUTCMinutes()) + ':' + toWidth(2,d.getUTCSeconds());
     }
+    */
 
     function scrollSlightly(bx, by) {
       var T = endTime - curTime();
       if(T<0) { alert("Done scrolling; you should be done reading!"); return; }
       var tx = window.scrollX, ty = window.scrollY;
-      var x = tx + (eps/T)*(bx-tx);
-      var y = ty + (eps/T)*(by-ty);
+      var x = Math.ceil(tx + (eps/T)*(bx-tx));
+      var y = Math.ceil(ty + (eps/T)*(by-ty) + 0.8); //Attempt to fix the increasing rate
       window.scrollTo(x,y);
       document.getElementById('scrspbutton').value = '' + ((eps/T)*(by-ty)).toFixed(2);
       //document.getElementById('scrspminutes').value= printTime(T);
-      things_to_do.push(function() { scrollSlightly(bx, by); }); //Let's not setTimeout ourselves
+      things_to_do.push(function() { scrollSlightly(bx, by); });
     }
 
     function make_box() {
@@ -96,14 +114,11 @@ if(window===window.top) {
         return false;
       }
       document.getElementById('scrspbutton').addEventListener('click', setAndScroll, true);
+      pop_queue();
     }
 
     gm_registerMenuCommand('Set_bottom',
-                           function() {
-                             gm_log('Setting...');
-                             make_box();
-                             pop_queue();
-                           },
+                           make_box,
                            'b', 'control alt');
 
   })();
@@ -111,7 +126,7 @@ if(window===window.top) {
  }
 
 /*
-In Firebug:
+Manually, in Firebug:
 
 function curTime() { return (new Date()).getTime(); }
 function scrollSlightly(bx, by) {T = endTime - curTime(); if(T<0) { alert("Done scrolling; you should be done reading!"); return; } var tx = window.scrollX, ty = window.scrollY; var eps = 100; var x = tx + (eps/T)*(bx-tx); var y = ty + (eps/T)*(by-ty); window.scrollTo(x,y); window.setTimeout(scrollSlightly, eps, bx, by);}
